f5faed3adb39390ebb2b0ca798b58750ae0871d5
AuroraTea <1352685369@qq.com>
2021-04-21 10:35:35 +0800

    甜糖白桃可爱风立体字时钟21.04.20
    https://www.bilibili.com/video/BV1SK411F77B/

---
+++ 7-LovelyClock/clock.js
@@ -0,0 +1,17 @@
+function myTime() {
+  let time = new Date();
+  let hh = time.getHours();
+  let mm = time.getMinutes();
+  let ss = time.getSeconds();
+
+  document.getElementById("1").innerText = Math.floor(hh / 10);
+  // 因为js的/不是整除而是得到浮点数
+  // 另外如果是喜欢看8:00而非08:00的这里可以做个判断,比如:
+  // document.getElementById("1").innerText = hh<10?null:Math.floor(hh / 10);
+  document.getElementById("2").innerText = hh % 10;
+  document.getElementById("4").innerText = Math.floor(mm / 10);
+  document.getElementById("5").innerText = mm % 10;
+  document.getElementById("7").innerText = Math.floor(ss / 10);
+  document.getElementById("8").innerText = ss % 10;
+}
+setInterval(myTime,1000);
