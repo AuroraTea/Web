f9f039be700d89b779c21ac9333da5210aa967ee
AuroraTea <1352685369@qq.com>
2021-06-21 10:49:24 +0800

    login-body的滑动效果

--- 10-aliDriverLogin/script.js
+++ 10-aliDriverLogin/script.js
@@ -1,2 +1,19 @@
 // 纯CSS计划告吹
-let message_login =document.getElementsByName(change-form);
+let login_opt =document.getElementsByName('change-form');
+let form_bar = document.getElementById('form-bar')
+
+
+function check(){
+  if (login_opt[0].checked){
+    form_bar.style.transform = 'translateX(0)';
+    console.log('1');
+  }
+  if (login_opt[1].checked){
+    form_bar.style.transform = 'translateX(-348px)';
+    console.log('2');
+  }
+  if (login_opt[2].checked){
+    form_bar.style.transform = 'translateX(-692px)';
+    console.log('3');
+  }
+}
