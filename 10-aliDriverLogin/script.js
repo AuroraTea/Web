ab81b4f1140619e2d9a49797ae070db77de32a98
AuroraTea <1352685369@qq.com>
2021-06-22 01:14:39 +0800

    代码整理

--- 10-aliDriverLogin/script.js
+++ 10-aliDriverLogin/script.js
@@ -6,14 +6,11 @@ let form_bar = document.getElementById('form-bar')
 function check(){
   if (login_opt[0].checked){
     form_bar.style.transform = 'translateX(0)';
-    console.log('1');
   }
-  if (login_opt[1].checked){
+  else if (login_opt[1].checked){
     form_bar.style.transform = 'translateX(-348px)';
-    console.log('2');
   }
-  if (login_opt[2].checked){
+  else{
     form_bar.style.transform = 'translateX(-692px)';
-    console.log('3');
   }
 }
