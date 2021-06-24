cf2218820d56b88587c9c978bc57321d1f70144a
AuroraTea <1352685369@qq.com>
2021-06-25 01:35:00 +0800

    优化js代码， 增加一种选择

--- 10-aliDriverLogin/script.js
+++ 10-aliDriverLogin/script.js
@@ -1,16 +1,22 @@
 // 纯CSS计划告吹
-let login_opt =document.getElementsByName('change-form');
+let login_opt = document.getElementsByName('change-form');
 let form_bar = document.getElementById('form-bar')
 
 
-function check(){
-  if (login_opt[0].checked){
+function check0() {
+  if (login_opt[0].checked) {
     form_bar.style.transform = 'translateX(0)';
-  }
-  else if (login_opt[1].checked){
+  } else if (login_opt[1].checked) {
     form_bar.style.transform = 'translateX(-348px)';
-  }
-  else{
+  } else {
     form_bar.style.transform = 'translateX(-692px)';
+  }
+}
+
+function check() {
+  for (let i = 0; i < login_opt.length; ++i) {
+    if (login_opt[i].checked) {
+      form_bar.style.transform = 'translateX(' + (-348 * i) + 'px)';
+    }
   }
 }
